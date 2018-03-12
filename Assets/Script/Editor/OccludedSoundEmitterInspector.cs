576d34c186668bb08772d4bd3aced6208d535b2c
oyar <hamuyrodrigo@gmail.com>
2018-03-12 17:24:14 +0000

    Working: Fishes physics

--- Assets/Script/Editor/OccludedSoundEmitterInspector.cs
+++ Assets/Script/Editor/OccludedSoundEmitterInspector.cs
@@ -5,15 +5,19 @@ using UnityEditor;
 
 [CustomEditor(typeof(OccludedSoundEmitter))]
 public class OccludedSoundEmitterInspector : Editor  {
+
+	bool showOcclussionRay = false;
 	
 	void OnSceneGUI() {	
+
+		if ( ! showOcclussionRay ) return;
 
 		var targetObject = (OccludedSoundEmitter) target;
 		Debug.DrawLine( targetObject.transform.position, Camera.main.transform.position, Color.red );
 
 		foreach( var anchor in targetObject.anchors ) {
 
-			Debug.DrawLine( anchor.position, Camera.main.transform.position, Color.red );
+			Debug.DrawLine( anchor.position, Camera.main.transform.position, Color.green );
 		
 		}
 		
--- Assets/Script/SplineNavigator.cs
+++ Assets/Script/SplineNavigator.cs
@@ -10,15 +10,115 @@ public class SplineNavigator : MonoBehaviour {
 	[Range(1, 0.01f)]
 	public float speedFactor = 0.1f;
 
-	float time = 0.0f;
+	[HideInInspector]
+	public float time;
+
+	public bool usePhysics = true;
+
+	[HideInInspector]
+	public Rigidbody rigidbody;
+
+	[HideInInspector]
+	public float physicTimeStep;
+
+	void Start() {
+
+		time = 0.0f;
+		physicTimeStep = 0.01f;
+
+		rigidbody = GetComponent<Rigidbody>();
+
+		var pos = spline.getPoint( time );
+
+		rigidbody.transform.position = pos;
+
+		var forward = (spline.getPoint( time + 0.01f ) - pos ).normalized;
+
+		rigidbody.transform.forward = forward;
+
+	}
 	
 	void FixedUpdate() {
 
+		if( usePhysics ) {
+
+			UpdatePhysics();
+
+		} else {
+
+			UpdateTransform();
+
+		}
+
+	}
+
+	void UpdatePhysics() {
+
+		print( time );
+
+		var maxSpeed = 5.0f;
+
+		if( rigidbody.velocity.magnitude < maxSpeed ) {
+
+			rigidbody.AddRelativeForce( Vector3.forward );
+
+		}
+
+		var currSplinePoint = spline.getPoint( time );
+		var nextSplinePoint = spline.getPoint( time + physicTimeStep );
+		var splineTangent = ( nextSplinePoint - currSplinePoint ).normalized;
+
+		var posToNextSplinePoint = nextSplinePoint - rigidbody.position;
+
+		var postToNextDOTTangent = Vector3.Dot( posToNextSplinePoint.normalized, splineTangent.normalized );
+
+		var rightDOTposToNextSplinePoint = Vector3.Dot( transform.right, posToNextSplinePoint );
+
+		var forwardDOTPosToNextSplinePoint = Vector3.Dot( transform.forward, posToNextSplinePoint.normalized);
+
+		// print( 
+		// 	"postToNextDotTangent: " + postToNextDOTTangent.ToString() 
+		// 	+ " - rightDotTangent: " + rightDOTTangent.ToString() 
+		// );
+
+		var torqueForce = 1.0f;
+		
+		rigidbody.AddRelativeTorque( Vector3.up * torqueForce * (rightDOTposToNextSplinePoint) );
+
+		if( forwardDOTPosToNextSplinePoint < 0.9f ) {
+
+			// // if is on the right
+			// if( rightDOTTangent > 0.0f ) {
+
+
+			// // if is on the left
+			// } else {
+
+			// 	rigidbody.AddRelativeTorque( - Vector3.up * torqueForce );
+
+			// }
+
+		}
+
+		var minDistance = 5.0f;
+
+		if( posToNextSplinePoint.magnitude < minDistance ) {
+			
+			time = (time + physicTimeStep) % 1;
+
+		}
+
+	}
+
+	void UpdateTransform() {
+
 		time = (time + Time.fixedDeltaTime * speedFactor) % 1;
+		var pos = spline.getPoint( time );
+		var forward = (spline.getPoint( time + 0.01f ) - pos ).normalized;
 
-		transform.position = spline.getPoint( time );
+		transform.position = pos;
 
-		transform.forward = (spline.getPoint( time + 0.01f ) - transform.position).normalized;
+		transform.forward = forward;
 
 	}
 }
