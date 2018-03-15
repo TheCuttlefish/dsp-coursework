6d4a55aaa9b10cc340aed9663f758cacc6bc3a74
RodrigoHamuy <hamuyrodrigo@gmail.com>
2018-03-15 15:06:38 +0000

    Working: Flocking

---
+++ Assets/Script/Editor/FlockAgentInspector.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+[CustomEditor(typeof(FlockAgent))]
+public class FlockAgentInspector : Editor {	
+	
+	void OnSceneGUI() {			
+
+		var obj = (FlockAgent) target;
+
+		if( obj.body == null ) return;
+
+		var crossRight = Vector3.Cross( obj.body.velocity.normalized, obj.transform.up );
+
+		var wallCollisionPoint = obj.getWallPointAt( obj.body.velocity.normalized );
+		var rightWall = obj.getWallPointAt( obj.body.velocity.normalized * 1.5f + crossRight.normalized );
+		var leftWall = obj.getWallPointAt( obj.body.velocity.normalized * 1.5f - crossRight.normalized );
+
+		VectorDebug forward = new VectorDebug {
+			start = obj.transform.position,
+			end = rightWall
+		};
+		VectorDebug right = new VectorDebug {
+			start = obj.transform.position,
+			end = wallCollisionPoint
+		};
+		VectorDebug left = new VectorDebug {
+			start = obj.transform.position,
+			end = leftWall
+		};
+
+		var color = Color.green;
+
+		if( forward.vector.magnitude < obj.wallLimit ) {
+
+			color = Color.red;
+
+		}
+
+		List<VectorDebug> list = new List<VectorDebug>(){
+			forward, left, right
+		};
+
+		list = list.OrderByDescending( a => a.vector.sqrMagnitude).ToList();
+
+		foreach( var item in list ){
+
+			color.a *= 0.5f;
+
+			Debug.DrawLine( item.start, item.end, color );
+
+		}
+
+	}
+
+}
+
+struct VectorDebug{
+
+	public Vector3 start;
+	public Vector3 end;
+
+	public Vector3 vector{
+		get { return start - end; }
+	}
+
+}
--- Assets/Script/FlockAgent.cs
+++ Assets/Script/FlockAgent.cs
@@ -6,26 +6,134 @@ public class FlockAgent : MonoBehaviour {
 
 	public Transform limitPoint;
 
-	float limit = 250.0f;
+	float outsideLimit = 250.0f;
 
+	[HideInInspector]
+	public float wallLimit = 200.0f;
+
+	float maxSpeed = 5.0f;
+	float torqueStrength = 0.1f;
+	float wallTorqueStrength = 4.0f;
 	
-	Rigidbody rigidbody;
+	[HideInInspector]
+	public Rigidbody body;
+
+	string[] layers = new string[] {
+		"Floor",
+		"Walls"
+	};
+
+	int layerMask;
+
+	Transform camera;
 
 	void Start () {
-		
-		rigidbody = GetComponent<Rigidbody>();
+
+		wallLimit = 500.0f;
+
+		Time.timeScale = 5.0f;		
+		body = GetComponent<Rigidbody>();
+		layerMask = LayerMask.GetMask( layers );
+		camera = Camera.main.transform;
 		
 	}
 
 	void FixedUpdate() {
 
-		var distanceToCenter = (transform.position - limitPoint.position).magnitude;
+		if( body.velocity.magnitude < maxSpeed ) {
 
-		if( distanceToCenter > limit ) {
+			body.AddRelativeForce( Vector3.forward, ForceMode.Impulse );
+
+		}
+
+		SceneConstrain();
+
+		WallConstrain();
+
+	}
+
+	void SceneConstrain() {
+
+		var vectorFromCenter = transform.position - limitPoint.position;
+
+		var distanceFromCenter = vectorFromCenter.magnitude;
+
+		// if it is outside the limit
+		if( distanceFromCenter > outsideLimit ) {
+
+			var dotToCenter = Vector3.Dot( 
+				body.velocity.normalized, 
+				vectorFromCenter.normalized 
+			);
+
+			var rightDot = Vector3.Dot( 
+				transform.right, 
+				- vectorFromCenter.normalized 
+			);
+
+			var torque = Vector3.up * torqueStrength * rightDot;
+
+			body.AddRelativeTorque( torque );
+
+			// // if its moving outside the limit
+			// if( dotToCenter > 0 ) {
+
+
+
+			// }
 
 			// You are too far!
 
 		}
 
 	}
+
+	void WallConstrain() {
+
+		var wallCollisionPoint = getWallPointAt(body.velocity.normalized);
+
+		var wallCollisionVector = wallCollisionPoint - transform.position;
+
+		var wallCollisionDistance = wallCollisionVector.magnitude;
+
+		if( wallCollisionDistance < wallLimit  && wallCollisionDistance != .0f ) {
+
+			var crossRight = Vector3.Cross( body.velocity.normalized, transform.up );
+
+			var rightWall = getWallPointAt( body.velocity.normalized * 1.5f + crossRight.normalized );
+			var leftWall = getWallPointAt( body.velocity.normalized * 1.5f - crossRight.normalized );
+
+			var rightWallVector = rightWall - transform.position;
+			var leftWallVector = leftWall - transform.position;
+
+			var torque = Vector3.up * wallTorqueStrength / wallCollisionDistance;
+
+			if( rightWallVector.sqrMagnitude > leftWallVector.sqrMagnitude ) {
+				
+				torque = - torque;
+
+			}
+
+			body.AddRelativeTorque( torque );
+
+		}
+
+	}
+
+	public Vector3 getWallPointAt( Vector3 direction ) {
+
+		RaycastHit hit;
+		var end = transform.position + direction * 9999.9f;
+		Physics.Linecast( transform.position, end, out hit, layerMask);
+
+		if( hit.collider != null ) {
+
+			return hit.point;
+
+		}
+
+		return end;
+		
+	}
+
 }
